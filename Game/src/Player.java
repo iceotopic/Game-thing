61b6f786bb56ec3c622f540d6e1b8eb97f48e143
vperezmende8326 <vperezmende8326@001LAB153010829.neisd.ad>
2016-02-01 11:59:08 -0600

    Initial commit

---
+++ Game/src/Player.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Player {
+	
+	{
+	String characterName;
+	Scanner scanner1 = new Scanner(System.in);
+
+	System.out.print("Enter character name   ===>>");
+	characterName = scanner1.nextLine();
+	}
+}
