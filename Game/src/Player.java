90c3dc2a73a412f7933914ed9bb050c4f90e11b6
vperezmende8326 <vperezmende8326@001LAB153010884.neisd.ad>
2016-02-03 12:00:20 -0600

    02/03/16

--- Game/src/Player.java
+++ Game/src/Player.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 public class Player {
 	
+	private int hp=50;
+	
+	public Player()
 	{
+	
 	String characterName;
 	Scanner scanner1 = new Scanner(System.in);
 
 	System.out.print("Enter character name   ===>>");
 	characterName = scanner1.nextLine();
 	
+	System.out.print("Hi");
+	System.out.print(characterName);
+	System.out.print(" ! ! !");
+	}
 	
+	
+	public int getHP()
+	{
+		return hp;
 	}
 }
