236f392ac6ae33530327e26506ff34f4c272ce5e
adaniels6539 <adaniels6539@001LAB153010827.neisd.ad>
2016-02-03 12:02:22 -0600

    Updated on 2-3-16

--- Game/src/Enemy.java
+++ Game/src/Enemy.java
@@ -1,6 +1,16 @@
 
 public class Enemy {
-int health = 50;
-int attack = 15;
+	int health = 30;
+	int attack = 15;
 
+	public void attack(Player p)
+	{
+		p.defend(attack);
+	}
+	
+	public void defend(int attackpoints) {
+		if (health == 0) {
+			System.out.println("The enemy's last words were 'Wubba Lubba Dub-dub...'");
+		}
+	}
 }
