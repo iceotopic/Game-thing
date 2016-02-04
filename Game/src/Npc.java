8f13306a1a0293a4ae9e24b647b38c897323285f
adaniels6539 <adaniels6539@001LAB153010827.neisd.ad>
2016-02-04 12:22:02 -0600

    NPC - Jessica change 2-4-16

--- Game/src/Npc.java
+++ Game/src/Npc.java
@@ -1,4 +1,24 @@
-
 public class Npc {
 //hello
+	
+	private String message;
+	
+	public Npc (String m)
+	{
+		message = m;
+	}
+	
+	public Npc()
+	{
+		message = "I don't know what to say";
+	}
+	
+	public String getMessage()
+	{
+		return message;
+	}
+	
+	
 }
+
+
