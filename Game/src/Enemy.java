5767c10ff540950f81a1930f4a790f3313e5fc21
adaniels6539 <adaniels6539@001LAB153010827.neisd.ad>
2016-02-05 12:20:10 -0600

    12:19 2-5-16

--- Game/src/Enemy.java
+++ Game/src/Enemy.java
@@ -2,12 +2,17 @@
 public class Enemy {
 	int health = 30;
 	int attack = 15;
-
+	
+	private int hp=50;
+	
 	public void attack(Player p)
 	{
 		p.defend(attack);
 	}
-	
+	public int getHP()
+	{
+		return hp;
+	}
 	public void defend(int attackpoints) {
 		if (health == 0) {
 			System.out.println("The enemy's last words were 'Wubba Lubba Dub-dub...'");
