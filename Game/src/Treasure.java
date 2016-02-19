aafdaae645cf85621a736af18272775ede82fb8d
lvelten8923 <lvelten8923@001LAB153010823.neisd.ad>
2016-02-19 11:43:14 -0600

    treasure

--- Game/src/Treasure.java
+++ Game/src/Treasure.java
@@ -2,14 +2,7 @@ import java.util.Random;
 
 public class Treasure {
 
-	/*These are the items that are given to the player throughout the game 
-	{
-	String type; 
-	String rewardtype;
-	String Chromosomes: This is what the player is rewarded with upon completing tasks or doing anything profitable
-	Int.reward amt (amount): This is the amount of chromosomes the player receives upon completing a profitable task
-	}
-	*/
+	
 	//
 	String[] types = {"Chromosome Cluster", "Weapon", "Ammunition Clip"};
 	String rewardType;
@@ -28,10 +21,11 @@ public class Treasure {
 
 	}
 	
-	public void display ()
+	public String toString ()
 	{
-		System.out.println("The " + rewardType + "gives you " + Chromosomes + " Chromosomes.");
+		return ("The " + rewardType + "gives you " + Chromosomes + " Chromosomes.");
 	}
 	
-
+	
+	
 }
