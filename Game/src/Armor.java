f911889339adeea5d00f93dd3793d1f3b492500f
cmartinez1409 <cmartinez1409@stu.neisd.ad>
2016-02-24 11:43:47 -0600

    changes huppen

--- Game/src/Armor.java
+++ Game/src/Armor.java
@@ -29,7 +29,7 @@ public class Armor {
 			chromosomePoints = 30;
 		}
 	}
-	public int damagePoints()
+	public int armorPoints()
 	{
 		return armorPoints - attackPoints;
 	}
--- Game/src/Weapon.java
+++ Game/src/Weapon.java
@@ -1,3 +1,4 @@
+
 public class Weapon {
 
 	int attackPoints;
